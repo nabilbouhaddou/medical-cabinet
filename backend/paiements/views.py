8fae873a8c30ea954997b0d7f81ec47ca107e46b
nabilbouhaddou <nabilbouhaddou4@gmail.com>
2026-04-29 21:20:49 +0100

    Update views.py

--- backend/paiements/views.py
+++ backend/paiements/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Paiement
 from .serializers import PaiementSerializer
 
+
 class PaiementViewSet(ModelViewSet):
-    queryset = Paiement.objects.all()
     serializer_class = PaiementSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        # 👨‍💻 Admin voit tout
+        if user.role == "admin":
+            return Paiement.objects.all()
+
+        # 👨‍⚕️ Médecin voit paiements de ses consultations
+        if user.role == "medecin":
+            return Paiement.objects.filter(consultation__medecin__user=user)
+
+        # 👤 Patient voit seulement ses paiements
+        if user.role == "patient":
+            return Paiement.objects.filter(consultation__patient__user=user)
+
+        return Paiement.objects.none()

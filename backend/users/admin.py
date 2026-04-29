1213ce4e91450fbcb87e5f4cf6914db91829527f
nabilbouhaddou <nabilbouhaddou4@gmail.com>
2026-04-29 17:23:30 +0100

    Update admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role', 'is_active')
+    list_filter = ('role', 'is_active')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
